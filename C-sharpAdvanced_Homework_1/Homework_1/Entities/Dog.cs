89787251ef0de90f85d5941458bfe8ea435e45c9
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2023-05-04 22:36:33 +0200

    Add files via upload

---
+++ C-sharpAdvanced_Homework_1/Homework_1/Entities/Dog.cs
@@ -0,0 +1,41 @@
+﻿using Homework_1.FileSystemHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1.Entities
+{
+    public class Dog
+    {
+        public Dog(string name, int age, string color)
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public static void PrintAllDogs(string jsonFile)
+        {
+            if (File.Exists(jsonFile))
+            {
+                string jsonDogs = ReaderWriter.ReadFile(jsonFile);
+                List<Dog> dogs = JsonConvert.DeserializeObject<List<Dog>>(jsonDogs);
+                foreach (Dog dog in dogs)
+                {
+                    Console.WriteLine($"Name: {dog.Name}, Age: {dog.Age}, Color: {dog.Color}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are not dogs in this file.");
+            }
+        }
+    }
+}
---
+++ C-sharpAdvanced_Homework_1/Homework_1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Create a Dog class with :
+
+//● Name, Age, Color
+//● Let the user input these parameters from a console application
+//● Create a new Dog object from the inputs and write it as a json in
+//a new file on the file system
+//● Create a method that reads and prints in the console all the dogs
+//from the json file
+
+using Homework_1;
+using Homework_1.Entities;
+using Newtonsoft.Json;
+using Homework_1.FileSystemHelper;
+
+string folderPath = @"..\..\..\Dogs";
+string filePath = folderPath + @"\dogsJson.json";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}   
+
+Console.WriteLine("Enter dog name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter dog age:");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter dog color:");
+string color = Console.ReadLine();
+
+
+Dog dog = new Dog(name, age, color);
+
+string jsonDog = JsonConvert.SerializeObject(dog);
+
+ReaderWriter.WriteFile(filePath, jsonDog);
+
+
+Dog.PrintAllDogs(filePath);
+
